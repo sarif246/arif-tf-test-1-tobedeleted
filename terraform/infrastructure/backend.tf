22684251706c05479f28330be4eb824a09a7e08a
sarif246 <sarif246@gmail.com>
2023-09-27 17:04:03 +0530

    Update backend.tf

--- terraform/infrastructure/backend.tf
+++ terraform/infrastructure/backend.tf
@@ -2,7 +2,7 @@ terraform {
   cloud {
     organization = "test-org-arif"
     workspaces {
-      prefix = "arif-tf-test-1-tobedeleted"
+      prefix = "arif-tf-test-1-tobedeleted.bk"
     }
   }
 }
