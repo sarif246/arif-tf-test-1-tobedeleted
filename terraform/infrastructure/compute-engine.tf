3af374ecff64a5c578e62634cddb094658b8ca3f
arif <arif@example.com>
2023-09-27 13:20:31 +0530

    c2

--- terraform/infrastructure/compute-engine.tf
+++ terraform/infrastructure/compute-engine.tf
@@ -1,5 +1,5 @@
 resource "google_compute_instance" "default" {
-  name         = "test-arif"
+  name         = var.name #"test-arif"
   machine_type = "e2-medium"
   zone         = "us-central1-a"
 
